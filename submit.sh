a5f80eb91e03b2345500de10c69e17edc9974f0e
gongjiaqi <Jiaqi.gong@mail.utoronto.ca>
2021-07-13 10:51:12 -0400

    Update submit.sh for running on the Compute Canada

--- submit.sh
+++ submit.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
+#SBATCH --mem=1024M
+
+#SBATCH --mail-user=jiaqi.gong@mail.utoronto.ca
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+#SBATCH --mail-type=ALL
+
 
 python3 RunSimulationScript.py
