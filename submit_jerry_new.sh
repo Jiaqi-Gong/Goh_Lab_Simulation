ef350d3be18613b14395b0f9e984d6486e568665
gongjiaqi <Jiaqi.gong@mail.utoronto.ca>
2021-07-14 18:48:09 -0400

    Update shell for Jerry

---
+++ submit_jerry_new.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --account=def-aspuru
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+#SBATCH --time=08:00:00
+#SBATCH --mem=128G
+
+#SBATCH --mail-user=jerryjh.gu@mail.utoronto.ca
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+#SBATCH --mail-type=ALL
+
+python3 RunSimulationScript.py
