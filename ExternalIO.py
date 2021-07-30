86956395144ac95e6d2075981951304b21f6f86a
rei289 <katsurei225@gmail.com>
2021-07-29 22:57:06 -0400

    changed size of marker

--- ExternalIO.py
+++ ExternalIO.py
@@ -11,13 +11,8 @@ from numpy import ndarray
 from openpyxl.packaging import workbook
 import time
 
-import vispy
-# vispy.use("osmesa")
-from vispy import app, visuals, scene, gloo
-import vispy.io as io
-import sys
 import matplotlib as mpl
-mpl.use ('Agg')
+mpl.use('Agg')
 import matplotlib.pyplot as plt
 
 LOG_CACH = []
@@ -25,77 +20,6 @@ LOG_CACH = []
 # INDICATOR record three bool
 # first is generate image or not, second is generate log or not, third is write log at last or not
 INDICATOR = [False, False, False]
-
-FRAGMENT_SHADER = """
-    // Extensions for WebGL
-    #extension GL_OES_standard_derivatives : enable
-    #extension GL_OES_element_index_uint : enable
-    #include "misc/spatial-filters.frag"
-    // Adapted from glumpy with permission
-    const float M_SQRT1_2 = 0.707106781186547524400844362104849039;
-    uniform sampler2D u_font_atlas;
-    uniform vec2 u_font_atlas_shape;
-    varying vec4 v_color;
-    uniform float u_npix;
-    varying vec2 v_texcoord;
-    const float center = 0.5;
-    float contour(in float d, in float w)
-    {
-        return smoothstep(center - w, center + w, d);
-    }
-    float sample(sampler2D texture, vec2 uv, float w)
-    {
-        return contour(texture2D(texture, uv).r, w);
-    }
-    void main(void) {
-        vec2 uv = v_texcoord.xy;
-        vec4 rgb;
-        // Use interpolation at high font sizes
-        if(u_npix >= 50.0)
-            rgb = CatRom(u_font_atlas, u_font_atlas_shape, uv);
-        else
-            rgb = texture2D(u_font_atlas, uv);
-        float distance = rgb.r;
-        // GLSL's fwidth = abs(dFdx(uv)) + abs(dFdy(uv))
-        float width = 0.5 * fwidth(distance);  // sharpens a bit
-        // Regular SDF
-        float alpha = contour(distance, width);
-        if (u_npix < 30.) {
-            // Supersample, 4 extra points
-            // Half of 1/sqrt2; you can play with this
-            float dscale = 0.5 * M_SQRT1_2;
-            vec2 duv = dscale * (dFdx(v_texcoord) + dFdy(v_texcoord));
-            vec4 box = vec4(v_texcoord-duv, v_texcoord+duv);
-            float asum = sample(u_font_atlas, box.xy, width)
-                       + sample(u_font_atlas, box.zw, width)
-                       + sample(u_font_atlas, box.xw, width)
-                       + sample(u_font_atlas, box.zy, width);
-            // weighted average, with 4 extra points having 0.5 weight
-            // each, so 1 + 0.5*4 = 3 is the divisor
-            alpha = (alpha + 0.5 * asum) / 3.0;
-        }
-        gl_FragColor = vec4(v_color.rgb, v_color.a * alpha);
-    }
-    """
-VERTEX_SHADER = """
-    attribute float a_rotation;  // rotation in rad
-    attribute vec2 a_position; // in point units
-    attribute vec2 a_texcoord;
-    attribute vec3 a_pos;  // anchor position
-    varying vec2 v_texcoord;
-    varying vec4 v_color;
-    void main(void) {
-        // Eventually "rot" should be handled by SRTTransform or so...
-        mat4 rot = mat4(cos(a_rotation), -sin(a_rotation), 0, 0,
-                        sin(a_rotation), cos(a_rotation), 0, 0,
-                        0, 0, 1, 0, 0, 0, 0, 1);
-        vec4 pos = $transform(vec4(a_pos, 1.0)) +
-                   vec4($text_scale(rot * vec4(a_position, 0.0, 1.0)).xyz, 0.0);
-        gl_Position = pos;
-        v_texcoord = a_texcoord;
-        v_color = $color;
-    }
-    """
 
 def getHelp() -> Dict[str, str]:
     """
@@ -295,9 +219,7 @@ def _visPlot2D(array: ndarray, picName: str) -> None:
     now = datetime.now()
     day = now.strftime("%m_%d")
     current_time = now.strftime("%H_%M_%S")
-    # initialize the pandas dataframe
-    # column_names = ['X', 'Y', 'Legend']
-    # df = pd.DataFrame(columns=column_names)
+
 
     pos = np.where(array == 1)
     neu = np.where(array == 0)
@@ -312,10 +234,22 @@ def _visPlot2D(array: ndarray, picName: str) -> None:
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
+    # define factor
+    factor = 100 / int(max(array.shape[0], array.shape[1]))
 
-    ax.scatter(pos_x, pos_y, c='blue', label='pos')
-    ax.scatter(neu_x, neu_y, c='green', label='neu')
-    ax.scatter(neg_x, neg_y, c='red', label='neg')
+    if 'whole_film' in picName:
+        # set title
+        name = "Surface of Film"
+        size = 5*factor
+
+    else:
+        # set title
+        name = 'Surface of Bacteria'
+        size = 5*factor
+
+    ax.scatter(pos_x, pos_y, c='blue', label='pos', s=size)
+    ax.scatter(neu_x, neu_y, c='green', label='neu', s=size)
+    ax.scatter(neg_x, neg_y, c='red', label='neg', s=size)
 
     ax.legend(loc="upper right")
     ax.set_xlabel("X")
@@ -334,101 +268,6 @@ def _visPlot2D(array: ndarray, picName: str) -> None:
 
     plt.imshow(array, interpolation='nearest')
 
-    #
-    # # build your visuals, that's all
-    # Scatter3D = scene.visuals.create_visual_node(visuals.MarkersVisual)
-    #
-    # # The real-things : plot using scene
-    # # build canvas
-    # canvas = scene.SceneCanvas(title="{}".format(picName), keys='interactive', show=True, bgcolor="white", dpi=128)
-    # view = canvas.central_widget.add_view()
-    #
-    # # for neutral
-    # neu = np.where(array == 0)
-    # neu_y = neu[0]
-    # neu_x = neu[1]
-    #
-    # n_neu = len(neu_y)
-    # c_neu = len(neu_y)
-    # position_neu = np.zeros((n_neu, 2))
-    # colors_neu = np.zeros((c_neu, 4))
-    # for i in range(n_neu):
-    #     # green
-    #     x = neu_x[i]
-    #     y = neu_y[i]
-    #     position_neu[i] = x, y
-    #     colors_neu[i] = (0, 1, 0, 0.8)
-    #
-    # # for positive
-    # pos = np.where(array == 1)
-    # pos_y = pos[0]
-    # pos_x = pos[1]
-    #
-    # n_pos = len(pos_y)
-    # c_pos = len(pos_y)
-    # position_pos = np.zeros((n_pos, 2))
-    # colors_pos = np.zeros((c_pos, 4))
-    #
-    # for i in range(n_pos):
-    #     # blue
-    #     x = pos_x[i]
-    #     y = pos_y[i]
-    #     position_pos[i] = x, y
-    #     colors_pos[i] = (0, 0, 1, 0.8)
-    #
-    # # for negative
-    # neg = np.where(array == -1)
-    # neg_y = neg[0]
-    # neg_x = neg[1]
-    #
-    # n_neg = len(neg_y)
-    # c_neg = len(neg_y)
-    #
-    # position_neg = np.zeros((n_neg, 2))
-    # colors_neg = np.zeros((c_neg, 4))
-    #
-    # for i in range(n_neg):
-    #     # red
-    #     x = neg_x[i]
-    #     y = neg_y[i]
-    #     position_neg[i] = x, y
-    #     colors_neg[i] = (1, 0, 0, 0.8)
-    #
-    # # concatenate both color and position
-    # position = np.concatenate((position_neu, position_pos, position_neg))
-    # colors = np.concatenate((colors_neu, colors_pos, colors_neg))
-    # # plot ! note the parent parameter
-    # p1 = Scatter3D(parent=view.scene)
-    # # p1 = scene.visuals.Markers(fcode=FRAGMENT_SHADER)
-    # p1.set_gl_state('opaque', blend=True, depth_test=False)
-    #
-    # if 'whole_film' in picName:
-    #     size = 1
-    # else:
-    #     size = 10
-    #
-    # p1.set_data(position, face_color=colors, symbol='o', size=size, edge_width=0.5, edge_color=colors)
-    # view.add(p1)
-    #
-    # # Add a ViewBox to let the user zoom/rotate
-    # view.camera = 'turntable'
-    # view.camera.fov = 45
-    # view.camera.distance = int(max(2*array.shape[1], 2*array.shape[0]))
-    # view.camera.center = (int(array.shape[1] / 2), int(array.shape[0] / 2))
-    #
-    # # plot XYZ axes
-    # # create a factor number that takes the size of the array and determines the size for font, and tick length
-    # factor = max(array.shape[0], array.shape[1])/10
-    #
-    # xax = scene.visuals.Axis(pos=[[0, 0], [int(array.shape[1]), 0]], domain=(0, int(array.shape[1])),
-    #                          tick_direction=(0, -1), axis_color='black', tick_color='black', text_color='black',
-    #                          font_size=128*factor, minor_tick_length=100*factor, major_tick_length=200*factor, axis_label='X axis',
-    #                          axis_font_size=128*factor, tick_label_margin=300*factor, axis_label_margin=800*factor, parent=view.scene)
-    # yax = scene.visuals.Axis(pos=[[0, 0], [0, int(array.shape[0])]], domain=(0, int(array.shape[0])),
-    #                          tick_direction=(-1, 0), axis_color='black', tick_color='black', text_color='black',
-    #                          font_size=128*factor, minor_tick_length=100*factor, major_tick_length=200*factor, axis_label='Y axis',
-    #                          axis_font_size=128*factor, tick_label_margin=300*factor, axis_label_margin=800*factor, parent=view.scene)
-
     global picFolder
     if "picFolder" not in globals():
         # save the image
@@ -440,42 +279,7 @@ def _visPlot2D(array: ndarray, picName: str) -> None:
             os.mkdir(picFolder)
 
     picPath = "{}/{}".format(picFolder, picName)
-    if 'film' in picName:
-        # set title
-        name = "Surface of Film"
 
-        # # set camera angle
-        # elevation = 90
-        # azimuth = 0
-        # view.camera.elevation = elevation
-        # view.camera.azimuth = azimuth
-        # image = canvas.render(bgcolor='white')[:, :, 0:3]
-        #
-        # # save file
-        # io.write_png('{}/{}.png'.format(picFolder, picName), image)
-
-    elif 'bacteria' in picName:
-        # set title
-        name = 'Surface of Bacteria'
-        # txt = scene.visuals.Text(text=name, font_size=256*factor, pos=(int(array.shape[1]/2), int(3*array.shape[0]/2)),
-        #                                                                parent=view.scene)
-        # view.add(txt)
-        #
-        # # set camera angle
-        # elevation = 90
-        # azimuth = 0
-        # view.camera.elevation = elevation
-        # view.camera.azimuth = azimuth
-        # image = canvas.render(bgcolor='white')[:, :, 0:3]
-        #
-        # # save file
-        # io.write_png('{}/{}.png'.format(picFolder, picName), image)
-    """
-    run
-    """
-    # if sys.flags.interactive != 1:
-    #     app.run()
-    # fig.update_layout(title=name)
     plt.savefig(picPath, dpi=300, bbox_inches='tight')
     endTime = time.time()
     totalTime = endTime - startTime
