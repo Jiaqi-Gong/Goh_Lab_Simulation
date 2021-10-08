2f6fd3cb110ed3eab790f0a31887f1d18ebeffed
rei289 <katsurei225@gmail.com>
2021-10-07 23:56:29 -0400

    added way of checking efficiency

--- SurfaceGenerator/Domain.py
+++ SurfaceGenerator/Domain.py
@@ -267,12 +267,12 @@ class DomainGenerator:
                 ncpus = cpu_count()
 
             # when the cpu number is less than 16, we will set the number of usable cpu to that number
-            if ncpus <= 25:
+            if ncpus <= 16:
                 cpu_number = ncpus
 
             # if the cpu number is greater than 16, we will just use 16 cpus to make things simple
             else:
-                cpu_number = 25
+                cpu_number = 16
 
             showMessage(f"number of CPUs is {ncpus} but we will use {cpu_number}")
 
@@ -512,6 +512,10 @@ class DomainGenerator:
         'if running on the computer, uncomment time.sleep to not make computer laggy (optional)'
         # time.sleep(1)
 
+        # initialize of times randomPoint is called
+        # [incorrect, correct]
+        rP = [0,0]
+
         # start to generate the domain on surface
         # to generate the domains on the surface, we will be using multiprocessing to take advantage of all 4 CPUS
         while generated < domainNumEach:
@@ -523,7 +527,12 @@ class DomainGenerator:
 
             # check the position of this shape is empty, if not empty, then continue
             if not checkEmpty(newSurface, domainWidth, domainLength, start, possible_charge):
+                # add to incorrect
+                rP[0] += 1
                 continue
+
+            # add to correct
+            rP[1] += 1
 
             # we will first generate all the domainNumChar1
             if domainNumChar1 > totalDomainChar[0]:
@@ -548,6 +557,10 @@ class DomainGenerator:
             # if its running for too long, that means we most likely reached the maximum amount of domains on the surface and
             # end the while loop
             timeout = time.time() + WAIT_TIME  # 60 seconds from now
+
+        # determine efficiency of randomPoint
+        efficiency = (rP[1] / sum(rP)) * 100
+        showMessage(f"Efficiency of _randomPoint function was {efficiency}")
 
         # combine the new surface and total number of domains generated into a list
         surface_generated = [newSurface, generated]
