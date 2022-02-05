3d8e9e9d2c3fa98ba584f07dd1015abe93007087
Jiaqi Gong <jiaqi@cedar5.cedar.computecanada.ca>
2022-02-05 15:14:40 -0800

    udate

--- submit_Jiaqi.sh
+++ submit_Jiaqi.sh
@@ -2,7 +2,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=21
 #SBATCH --time=24:00:00
-#SBATCH --mem=10G
+#SBATCH --mem=15G
 
 #SBATCH --mail-user=jiaqi.gong@mail.utoronto.ca
 #SBATCH --mail-type=BEGIN
